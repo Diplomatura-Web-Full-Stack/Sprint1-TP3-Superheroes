78c66fb649d8c75c35a3eb6d0def0c4c82c37fa7
AbiZur <abigailzurita.dazr@gmail.com>
2025-03-13 12:00:51 -0300

    actualizacion index

--- superheroes/index.mjs
+++ superheroes/index.mjs
@@ -1,7 +1,14 @@
-import { leerSuperheroes } from './utils.mjs';
+import { agregarSuperheroes, leerSuperheroes } from './utils.mjs';
 
 //leer y mostrar la lista de superheroes ordenada
-const superheroes=leerSuperheroes('./superheroes.txt');
+const archivoOriginal = './superheroes.txt' ;
+const archivoNuevos = './nuevosSuperheroes.txt';
+
+//agregar nuevos superheroes
+agregarSuperheroes(archivoOriginal, archivoNuevos);
+
+//ler y mostrar lista actualizada de superheroes ordenada
+const superheroes = leerSuperheroes(archivoOriginal);
 console.log('Superhéroes ordenados:');
 console.log(superheroes);
 
